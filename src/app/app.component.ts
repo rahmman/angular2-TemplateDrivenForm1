bad180ea4f204383b0e0a1a981ed00daa98a8826
rahmman <rahmman.cse@outlook.com>
2017-02-26 11:53:17 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import {UserInfo} from './user-info';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  user:UserInfo={
+    name: "",
+    account : {email: '',
+  confirm:''}
+};
+  onSubmit({value,valid}: {value:UserInfo, valid:boolean}){
+    console.log(value, valid)
+  }
 }
---
+++ src/app/user-info.ts
@@ -0,0 +1,5 @@
+export interface UserInfo {
+  name: string;
+account:{  email: string;
+  confirm: string}
+}
